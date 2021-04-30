199eb840b7c8a6bc4a1c05a95a90b79848b02331
AliyaDav <aliya_dav98@mail.ru>
2021-04-30 15:27:52 +0200

    Step functions added

--- model.py
+++ model.py
@@ -4,6 +4,9 @@
 # from mesa.datacollection import DataCollector
 
 from mesa import Agent, Model
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Agent(Country):
 
@@ -13,43 +16,41 @@ class Agent(Country):
         self.country = Country()
         p = np.random.random()
         if p > 0.5:
-            self.gender = 'female'
+            self.gender = 0 # female
         else:
-            self.gender = 'male'
-        self.gender
-        self.income = self.random.random(self.avg_income, .. ) # should be inherented from the country class - # google std of wage 
-        self.ambition = self.random.random() # function of age 
-        self.happy = 
-        
-    def willingness_to_move(self):
-        return np.exp(self.gender*0.2 + self.age + self.ambition - self.country.attachment + self.income) # change this 
-    
-    def decide_to_move(thresh):
-        if self.willingness_to_move() > thresh:
-            return True 
+            self.gender = 1 # male
+        self.income = np.random.random(self.country.avg_income, self.country.avg_income*0.1) # should be inherented from the country class - # google std of wage 
+        self.happy =
+        a = np.random.gamma(2,2,self.num_agents)
+        self.age = np.floor(a*45/max(a)+25)
+        self.ambition = 1/self.age # function of age 
 
-    def choose_country(self, countries):
+    def willingness_to_move(self):
+        return np.exp(self.gender*0.2 - self.age + self.ambition) / (np.exp(self.gender*0.2 - self.age + self.ambition)+1) # - self.country.attachment + self.income) # change this 
+    
+    def decide_to_move(thresh=0.3):
+        if self.willingness_to_move() > thresh:
+            return True
+
+    def choose_country(self, countries_dict):
         p = np.array()
-        for i, country in enumerate(countries):
-            c = Country(country)
+        for i, c in enumerate(countries_dict.values()):
             p[i] = c.gdp + ..
         p_scaled = p / p.sum()
         p_cumsum = p_scaled.cumsum()
         r = np.random.random()
         p_cumsum -= r
-        return countries[np.argmax(p_cumsum > 0)-1]
+        return list(countries_dict.keys())[np.argmax(p_cumsum > 0)-1]
+
+    def update_income(self):
+        self.income = np.random.random(self.country.avg_income, self.country.avg_income*0.1)
 
     def step(self):
-        similar = 0
-        for neighbor in self.model.grid.neighbor_iter(self.pos):
-            if neighbor.type == self.type:
-                similar += 1
 
-        # If unhappy, move:
-        if similar < self.model.homophily:
-            self.model.grid.move_to_empty(self)
-        else:
-            self.model.happy += 1
+        if self.decide_to_move():
+            self.country = countries_dict[self.choose_country()]
+            self.update_income()
+        self.age += 1
 
 
 class Country():
@@ -61,57 +62,40 @@ class Country():
         self.population = data['pop']
         self.average_income = data['avg_inc']
         self.hdi = data['hdi']
-        self.life_exp = data['life_exp'] 
-        self.attachment = data['attachment']
-        self.schedule = RandomActivation(self)
-        self.datacollector = DataCollector(
-            {"happy": "happy"},  # Model-level count of happy agents
-            # For testing purposes, agent's individual x and y
-            {"x": lambda a: a.pos[0], "y": lambda a: a.pos[1]},
-        )
+        self.life_exp = data['life_exp']
 
-        # Set up agents
-        # We use a grid iterator that returns
-        # the coordinates of a cell as well as
-        # its contents. (coord_iter)
-        for cell in self.grid.coord_iter():
-            x = cell[1]
-            y = cell[2]
-            if self.random.random() < self.density:
-                if self.random.random() < self.minority_pc:
-                    agent_type = 1
-                else:
-                    agent_type = 0
+        # policies to be defined 
 
-                agent = SchellingAgent((x, y), self, agent_type)
-                self.grid.position_agent(agent, (x, y))
-                self.schedule.add(agent)
-
-        self.running = True
-        self.datacollector.collect(self)
-
-
+        def attract_brains():
+            self.average_income *= 1.1
+        
+        def keep_brains():
+            
 
 class MigrationModel():
 
     def __init__(self, N):
+
         self.num_agents = N
+        self.running = True
+
         # Create agents
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            a = Agent(self)
+
+        # Create countries
+        for i in range(len(countries_dict)):
+            c = Country(df)
 
     def step(self):
         """
         Run one step of the model. If All agents are happy, halt the model.
         """
-        self.steps += 1  # Reset counter of happy agents
-        self.schedule.step()
-        self.datacollector.collect(self)
-
-        if self.happy == self.schedule.get_agent_count():
-            self.running = False
+        self.steps += 1 
+        self.
 
 
+# SAVE AND COMMIT 
 
     
 
